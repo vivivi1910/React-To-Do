3e8f3140f6458718ebb8196da8c6ef66c84f5809
vivivi1910 <vikulis@gmail.com>
2018-11-29 10:54:11 +0200

    ReactTo do list veikia su atskirais js

--- src/todo-app.js
+++ src/todo-app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class TodoApp extends React.Component {
     constructor(props) {
         super(props);
@@ -59,19 +61,7 @@ class TodoApp extends React.Component {
     }
 }
 
-class TodoList extends React.Component {
-    render() {
-        return React.createElement(
-            "ul",
-            null,
-            this.props.items.map(item => React.createElement(
-                "li",
-                { key: item.id },
-                item.text
-            ))
-        );
-    }
-}
+
 
 
 
